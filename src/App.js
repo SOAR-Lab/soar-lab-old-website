15ff012f5a0896e1341fe02b2ad627a8ca56acf1
yashasvi-jain <47085019+yashasvi-jain@users.noreply.github.com>
2022-03-08 11:14:52 -0500

    changed config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Compnents/Navbar/Navbar';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Publications from './Compnents/Publications';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+          <Routes>
+            <Route path='/' exact element={Home} />
+            <Route path='/Publications' element={<Publications />} />
+          </Routes>
+      </div>
+    </Router>
   );
+}
+
+const Home = () => {
+  <div>
+    <h1>HOME</h1>
+  </div>
 }
 
 export default App;
